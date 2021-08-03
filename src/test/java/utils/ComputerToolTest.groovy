bab3a2b29a0c16cf744d932a1c754467e1176feb
002080-孔晓明 <249434000@qq.com>
2021-08-03 14:07:39 +0800

    [002080][kongxiaoming][style] 格式化单元测试代码

--- src/test/java/utils/ComputerToolTest.groovy
+++ src/test/java/utils/ComputerToolTest.groovy
@@ -1,19 +1,18 @@
 package utils
 
 import spock.lang.Specification
-import spock.lang.Unroll
 
 class ComputerToolTest extends Specification {
     def "GetCommonDivisor"() {
         expect:
-        result == ComputerTool.getCommonDivisor(a,b)
+        result == ComputerTool.getCommonDivisor(a, b)
         where:
-        result |  a |  b
-        3      | 3  |  9
+        result | a  | b
+        3      | 3  | 9
         5      | 5  | -10
         1      | 5  | 6
         1      | 7  | -8
-        5      | 15  | 25
+        5      | 15 | 25
         1      | 0  | 10
     }
 }
