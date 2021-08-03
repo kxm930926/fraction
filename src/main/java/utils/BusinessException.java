35e55956cafeac50ff2ce1bc309b11a8d2e7275d
002080-孔晓明 <249434000@qq.com>
2021-08-03 15:30:02 +0800

    [002080][kongxiaoming][build] 增加业务异常类

---
+++ src/main/java/utils/BusinessException.java
@@ -0,0 +1,15 @@
+package utils;
+
+/**
+ * 业务异常类
+ */
+public class BusinessException extends Exception {
+
+    /**
+     * 业务异常
+     * @param message 异常信息
+     */
+    public BusinessException(String message){
+        super(message);
+    }
+}
